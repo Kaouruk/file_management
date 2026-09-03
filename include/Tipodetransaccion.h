5d5ea8d1c8eb945d07c51b16769f8d7068067fe4
Kaouruk <kathiaparedes02@gmail.com>
2026-09-02 18:31:41 -0600

    Add files via upload

---
+++ include/Tipodetransaccion.h
@@ -0,0 +1,16 @@
+#ifndef TIPODETRANSACCION_H
+#define TIPODETRANSACCION_H
+
+
+class Tipodetransaccion
+{
+    public:
+        Tipodetransaccion();
+        virtual ~Tipodetransaccion();
+
+    protected:
+
+    private:
+};
+
+#endif // TIPODETRANSACCION_H
